d0ae33bbbd3b5f0bc3a6219095c64347270689ba
JinYoung5 <143825200+JinYoung5@users.noreply.github.com>
2023-09-14 15:36:47 +0900

    9.14

--- src/kr/s01/exception/ExceptionMain04.java
+++ src/kr/s01/exception/ExceptionMain04.java
@@ -1,11 +1,20 @@
 package kr.s01.exception;
 
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class ExceptionMain04 {
 	
 	public void printData() throws IOException{
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("단 입력:");
+		int dan = Integer.parseInt(br.readLine());
+		System.out.println(dan + "단");
+		System.out.println("----------------");
+		for(int i=1;i<=9;i++) {
+			System.out.println(dan + "*" + i + "=" + dan*i);
+		}
 	}
 	public static void main(String[] args) {
 		ExceptionMain04 ex = new ExceptionMain04();
