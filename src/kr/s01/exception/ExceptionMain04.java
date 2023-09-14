15f4a399f80e3cb21e8e78cbfe972b4cbfd139d8
JinYoung5 <143825200+JinYoung5@users.noreply.github.com>
2023-09-14 15:46:52 +0900

    9.14

--- src/kr/s01/exception/ExceptionMain04.java
+++ src/kr/s01/exception/ExceptionMain04.java
@@ -6,6 +6,13 @@ import java.io.InputStreamReader;
 
 public class ExceptionMain04 {
 	
+	/*
+	 * throw 예약어의 사용
+	 * 메서드에 throws 예외 클래스를 명시하면 메서드 내에 try~catch블럭을 생략할 수 있고
+	 * 예외가 발생하면 예외를 보관해서 메서드를 호출한 곳에 try~catch블럭을 만들었을 경우
+	 * 그 곳으로 예외를 보냄
+	 */
+	
 	public void printData() throws IOException,NumberFormatException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.print("단 입력:");
