683583ea0daaa61843c294bf873f1e17bd25f62f
JinYoung5 <143825200+JinYoung5@users.noreply.github.com>
2023-09-14 15:25:25 +0900

    9.14

--- src/kr/s01/exception/ExceptionMain04.java
+++ src/kr/s01/exception/ExceptionMain04.java
@@ -1,11 +1,18 @@
 package kr.s01.exception;
 
+import java.io.IOException;
+
 public class ExceptionMain04 {
 	
-	public void printData() {
+	public void printData() throws IOException{
 		
 	}
 	public static void main(String[] args) {
-		
+		ExceptionMain04 ex = new ExceptionMain04();
+		try {
+			ex.printData();
+		}catch(IOException e) {
+			System.out.println("입력시 오류 발생");
+		}
 	}
 }
