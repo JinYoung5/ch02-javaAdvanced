6909aad51f29a977c9ed8a3dae36b17c7d9e6fd9
JinYoung5 <143825200+JinYoung5@users.noreply.github.com>
2023-09-14 17:17:03 +0900

    예외처리

---
+++ src/kr/s01/exception/ExceptionMain07.java
@@ -0,0 +1,31 @@
+package kr.s01.exception;
+
+import java.util.Scanner;
+
+//사용자 정의 예외 클래스
+class NegativeNumberUseException extends Exception{
+	public NegativeNumberUseException(String str) {
+		super(str);
+	}
+}
+
+public class ExceptionMain07 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("0이상만 입력:");
+		try {
+			int a = input.nextInt();
+			if(a>=0) {
+				System.out.println("입력한 숫자 : " + a);
+			}else {
+				throw new NegativeNumberUseException("음수는 사용할 수 없습니다.");
+			}
+		}catch(NegativeNumberUseException e) {
+			System.out.println(e.getMessage());
+		}catch(Exception e) {
+			System.out.println("예외 발생");
+		}finally {
+			if(input!=null) input.close();
+		}
+	}
+}
