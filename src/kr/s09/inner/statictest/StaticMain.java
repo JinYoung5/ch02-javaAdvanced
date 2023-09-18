019b8b918e0b18905b6e3bbcf6501dda3d23e29a
JinYoung5 <143825200+JinYoung5@users.noreply.github.com>
2023-09-18 16:19:03 +0900

    9.18 static inner class

---
+++ src/kr/s09/inner/statictest/StaticMain.java
@@ -0,0 +1,25 @@
+package kr.s09.inner.statictest;
+
+public class StaticMain {
+	
+	
+	//내부클래스에 static을 명시하면 내부클래스를 감싸고 있는 클래스를 객체생성하지 않고 내부클래스를 직접 객체 생성해서 사용가능
+	public static class StaticInner{
+		int iv = 200;
+		static  int cv = 300;
+		static void make() {
+			System.out.println("하하");
+		}
+	}
+	
+	public static void main(String[] args) {
+		StaticInner i = new StaticInner();
+		System.out.println(i.iv); //인스턴스 변수는 객체 생성 후 호출
+		
+		//static 변수
+		System.out.println(StaticInner.cv);
+		//static 메서드
+		StaticInner.make();
+		
+	}
+}
